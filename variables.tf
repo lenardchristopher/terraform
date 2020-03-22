cf21b6af102e807225a81da7e33bcc384408704a
Chris Lenard <christopher.lenard@relativity.com>
2020-03-22 18:52:59 +0000

    Fix typo

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-variable "service_principl_id" {
+variable "service_principal_id" {
   type = "string"
   description = "The id of the azure service principal for the Kubernetes cluster."
 }
 
-variable "service_principl_secret" {
+variable "service_principal_secret" {
   type = "string"
   description = "The secret of the azure service principal for the Kubernetes cluster."
 }
