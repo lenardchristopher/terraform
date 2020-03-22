4b02339ec120eac50a026733770fd1fe1db7ab4e
Chris Lenard <christopher.lenard@relativity.com>
2020-03-22 17:37:35 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,45 @@
+# Configure the Azure Provider
+provider "azurerm" {
+  version = "=2.0.0"
+  features {}
+}
+
+resource "azurerm_resource_group" "test_resource_group" {
+  name     = "test-rg"
+  location = "Central US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "test-cluster"
+  location            = azurerm_resource_group.test_resource_group.location
+  resource_group_name = azurerm_resource_group.test_resource_group.name
+  dns_prefix          = "clenard"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  service_principal {
+    client_id     = "00000000-0000-0000-0000-000000000000"
+    client_secret = "00000000000000000000000000000000"
+  }
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "generalstorage"
+  resource_group_name      = azurerm_resource_group.test_resource_group.name
+  location                 = azurerm_resource_group.test_resource_group.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+}
